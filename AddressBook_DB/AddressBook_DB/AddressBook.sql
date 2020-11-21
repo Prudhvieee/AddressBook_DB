78febb034c20382e866950fd709d6bebc595b8e1
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-21 10:49:37 +0530

    [Prudhvi] add . Creating address book table

--- AddressBook_DB/AddressBook_DB/AddressBook.sql
+++ AddressBook_DB/AddressBook_DB/AddressBook.sql
@@ -4,3 +4,17 @@ UC-1
 */
 create database AddressBook_Service;
 use AddressBook_Service;
+/*
+UC-2
+Creating Address book table
+*/
+create table AddressBook_Table
+( firstName varchar(50) not null,
+ secondName varchar(50) not null,
+ address varchar(50) not null,
+ city varchar(50) not null,
+ state varchar(50) not null,
+ zip bigint not null,
+ phoneNumber bigint not null,
+ emailId varchar(50) not null
+ );
