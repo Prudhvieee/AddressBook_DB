98f667592eca73f9e3c7e727d8921469e01d9560
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-21 11:16:02 +0530

    [Prudhvi] add . inserting data into table

--- AddressBook_DB/AddressBook_DB/AddressBook.sql
+++ AddressBook_DB/AddressBook_DB/AddressBook.sql
@@ -18,3 +18,16 @@ create table AddressBook_Table
  phoneNumber bigint not null,
  emailId varchar(50) not null
  );
+ /*
+ UC-3
+  Inserting contacts to AddressBook_Table.
+ */
+ insert into AddressBook_Table (firstName, secondName, address, city, state, zip, phoneNumber, emailId)
+ values
+ ('Prudhvi', 'Reddy', 'Vasavi Nagar', 'tpty', 'Andhra Pradesh', 9087654321, 517502, 'prudhvi@gmail.com'),
+ ('jerry', 'don', 'esteem street', 'Silicon valley', 'America', 9876543230, 500082, 'jerry@gmail.com'),
+ ('tom', 'Spinach', 'Mount street', 'SFO', 'CA', 3216547890, 98754, 'TOM@gmail.com');
+ /*
+  Displaying the contacts from table
+ */
+ select * from addressBook_Table
